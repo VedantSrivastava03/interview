6797c2c56a9774911e8c3e94b30c87c68fe0fbdd
Vedant Srivastava <vedsrivastava99@gmail.com>
2024-01-27 19:09:29 +0530

    first commit

---
+++ backend/mongo.js
@@ -0,0 +1,24 @@
+const mongoose=require("mongoose")
+mongoose.connect("mongodb+srv://admin:Srivastava%402024@cluster0.wpz9sry.mongodb.net/auth")
+.then(()=>{
+    console.log("mongodb connected");
+})
+.catch(()=>{
+    console.log('failed');
+})
+
+
+const newSchema=new mongoose.Schema({
+    email:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    }
+})
+
+const collection = mongoose.model("collection",newSchema)
+
+module.exports=collection
